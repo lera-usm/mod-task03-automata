b09c5ad049da4b9ed402af9fa058b1f34b139f57
Lera <lera.usmanova.2000@mail.ru>
2021-04-02 11:22:32 +0300

    381807-1 Usmanova Valeria

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@ int main()
 	vector<string> menu{ "capuchino","latte","mokko" };
 	vector<int> ceny{ 25,20,30 };
 	int sdacha = 0;
-	Automata cofe;
+	Automata cofe(menu,ceny);
 	cofe.on();
 	cofe.coin(20);
 	bool status=cofe.vibor(2);
